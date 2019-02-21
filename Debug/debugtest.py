a5cf2ce2b09ba38132a038aac7bb2caea03854e8
xiekaivaw <xiekaivaw@gmail.com>
2019-02-21 14:44:46 +0800

    add Debug File

---
+++ Debug/debugtest.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import unittest #加载单元测试模块
+
+class Student(object):
+
+    def __init__(self,name,score):
+        self.name = name
+        self.score = score
+        
+    def get_grade(self):
+        if self.score not in range(0,101):
+            raise ValueError(r"'Dict' objrct has no attribute '%s'" % self.score)
+        if self.score >= 80:
+            return 'A'
+        if self.score >= 60:
+            return 'B'
+        return 'C'
+
+
+class TestStudent(unittest.TestCase): #编写单元测试类
+
+    def test_80_to_100(self):
+        s1 = Student('Bart', 80)
+        s2 = Student('Lisa', 100)
+        self.assertEqual(s1.get_grade(), 'A')
+        self.assertEqual(s2.get_grade(), 'A')
+    
+    def test_60_to_80(self):
+        s1 = Student('Bart', 60)
+        s2 = Student('Lisa', 79)
+        self.assertEqual(s1.get_grade(), 'B')
+        self.assertEqual(s2.get_grade(), 'B')
+
+    def test_0_to_60(self):
+        s1 = Student('Bart', 0)
+        s2 = Student('Lisa', 59)
+        self.assertEqual(s1.get_grade(), 'C')
+        self.assertEqual(s2.get_grade(), 'C')
+
+    def test_invalid(self):
+        s1 = Student('Bart',-1)
+        s2 = Student('Lisa',101)
+        with self.assertRaises(ValueError):
+            s1.get_grade()
+        with self.assertRaises(ValueError):
+            s2.get_grade()
+
+
+if __name__ == '__main__':
+	unittest.main()
--- OOP/class_test.py
+++ OOP/class_test.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
-
+# 新建一个类
+'''
 class Student(object):
     def __init__(self, name, score):
         self.name = name
@@ -19,3 +20,86 @@ lisa = Student('Lisa', 98)
 bart = Student('Bart', 59)
 print(lisa.name, lisa.get_grade())
 print(bart.name, bart.get_grade())
+'''
+
+# 把下面的Student对象的gender字段对外隐藏起来，用get_gender()和set_gender()代替，并检查参数有效性：
+'''
+class Student(object):
+    def __init__(self,name,gender):
+        self.__name = name
+        self.__gender = gender
+    
+    def set_gender(self,gender):
+        if gender == 'male' or gender == 'female':
+            self.__gender = gender
+        else:
+             raise ValueError('error gender')
+
+    def get_gender(self):
+        return self.__gender
+
+# 测试:
+bart = Student('Bart', 'male')
+if bart.get_gender() != 'male':
+    print('测试失败!')
+else:
+    bart.set_gender('female')
+    if bart.get_gender() != 'female':
+        print('测试失败!')
+    else:
+        print('测试成功!')
+
+'''
+# 请利用@property给一个Screen对象加上width和height属性，以及一个只读属性resolution：
+'''
+class Screen(object):
+    @property
+    def width(self):
+        return self._width
+    @width.setter
+    def width(self, value):
+        self._width = value
+    
+    @property
+    def height(self):
+        return self._height
+    @height.setter
+    def height(self,value):
+        self._height = value
+
+    @property
+    def resolution(self):
+        return self._height * self._width
+# 测试:
+s = Screen()
+s.width = 1024
+s.height = 768
+print('resolution =', s.resolution)
+if s.resolution == 786432:
+    print('测试通过!')
+else:
+    print('测试失败!')
+
+'''
+# 实现一个Fib类用作可以 for ... in 循环
+'''
+class Fib(object):
+    def __init__(self):
+        self.a,self.b = 0,1
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        self.a, self.b = self.b, self.a + self.b
+        if self.a > 1000:
+            raise StopIteration()
+        return self.a
+
+for n in Fib():
+    print(n)
+'''
+from enum import Enum
+
+Month = Enum('Month1', ('Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'))
+for name, member in Month.__members__.items():
+    print(name,'=>',member,',',member.value)
